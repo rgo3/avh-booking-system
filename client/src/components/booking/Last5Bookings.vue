a078f3c8310903e877490841898ca1ece8ad8ad7
maseiler <matthiasseiler@outlook.com>
2019-08-01 19:16:56 +0200

    Implemented undoing of last 5 book entries

--- client/src/components/booking/Last5Bookings.vue
+++ client/src/components/booking/Last5Bookings.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
     <div class="list is-hoverable">
-      <a v-for="entry in bookings" :key="entry" class="list-item has-background-white">
-        {{printDateTime(entry.TimeStamp)}}
-        <br />
-        <div
-          v-if="entry.ItemID === 0"
-        >{{displayUserName(getUserByID(entry.UserID))}} ~ Payment: {{entry.TotalPrice * -1}}€</div>
-        <div
-          v-else
-        >{{displayUserName(getUserByID(entry.UserID))}} ~ {{entry.Amount}}x {{displayItem(getItemByID(entry.ItemID))}}</div>
-      </a>
+      <div v-for="entry in bookings" :key="entry" class="list-item has-background-white">
+        <div class="level">
+          <div class="level-left">
+            <h6 class="subtitle is-6">{{printDateTime(entry.TimeStamp)}}</h6>
+            <br />
+            <h6
+              class="subtitle is-6"
+              v-if="entry.ItemID === 0"
+            >{{displayUserName(getUserByID(entry.UserID))}} ~ Payment: {{entry.TotalPrice * -1}}€</h6>
+            <h6
+              class="subtitle is-6"
+              v-else
+            >{{displayUserName(getUserByID(entry.UserID))}} ~ {{entry.Amount}}x {{displayItem(getItemByID(entry.ItemID))}}</h6>
+          </div>
+          <!-- TODO timeout -->
+          <button class="button is-small" @click="undo(entry)">
+            <span class="icon is-small">
+              <font-awesome-icon icon="redo" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +32,26 @@ export default {
   computed: {
     bookings() {
       return this.$store.state.last5Bookings;
+    }
+  },
+  methods: {
+    undo(bookEntry) {
+      this.$http
+        .post("deleteBookEntry", bookEntry)
+        .then(() => {
+          this.$store.commit("getLast5Bookings");
+          var message = "".concat(
+            "Deleted book entry from ",
+            this.displayUserName(this.getUserByID(bookEntry.UserID))
+          );
+          this.$responseEventBus.$emit("successMessage", message);
+        })
+        .catch(() => {
+          this.$responseEventBus.$emit(
+            "failureMessage",
+            "Couldn't undo book entry"
+          );
+        });
     }
   }
 };
